fdd22d716027fc143ee6308f1127cd11cf714180
thanyapisit Buapakong <nyman3527@gmail.com>
2021-07-08 22:42:50 +0700

    Update fds.py

--- ทำโจทย์/fds.py
+++ ทำโจทย์/fds.py
@@ -1,16 +1,5 @@
-a = input()
-j = int(a)
-c = 0
-for s in a:
-    t = int(s)
-    c += t
-d = []
-for i in range(100000):
-    b = str(i)
-    e=0
-    for x in b:
-        y = int(x)
-        e += y
-    if j-i == c+e:
-        d.append(i)
-print(d[0])
+a = input().upper()
+g = 0
+for i in a:
+    if a.count(i) == 1:
+        g += 1
